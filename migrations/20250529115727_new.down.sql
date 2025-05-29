647586c2f56ac8562cda1c5fc40ec47e7393f595
Neko Sakura <sakura@nekocat.org>
2025-05-29 20:11:53 +0800

    SQL: Use SQLx migration files

---
+++ migrations/20250529115727_new.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE users;
+DROP TABLE server;
+DROP TABLE use_server;
+DROP TABLE logs;
+DROP TABLE ticket;
+DROP TABLE cart;
+DROP TABLE invoice;
+DROP TABLE payment;
+DROP TABLE setting;
+DROP TABLE announcement;
